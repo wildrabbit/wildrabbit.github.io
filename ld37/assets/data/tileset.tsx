65af315a82d0810babcc374798678a3fa1e069b6
wildrabbit <wildrabbit@users.noreply.github.com>
2016-12-18 20:54:33 +0000

    Add files via upload
    Updated HTML5 post-compo version

---
+++ ld37/assets/data/tileset.tsx
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="64" tileheight="64" tilecount="14" columns="0">
+ <tile id="0">
+  <properties>
+   <property name="edgeType" value="bottom"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/edge_bottom.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="edgeType" value="left"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/edge_left.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="edgeType" value="right"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/edge_right.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="edgeType" value="top"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/edge_top.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="tileType" value="turnLeft"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_bounceCCW_00.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="tileType" value="turnLeft"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_bounceCCW_01.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="tileType" value="turnRight"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_bounceCW_00.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="tileType" value="turnRight"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_bounceCW_01.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="tileType" value="void"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_empty.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="tileType" value="gap"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_gap_00.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="tileType" value="gap"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_gap_01.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="tileType" value="ground"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_ground_00.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="tileType" value="ground"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_ground_01.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="tileType" value="fullBlock"/>
+  </properties>
+  <image width="64" height="64" source="../../../art/tiles/images/tile_wall.png"/>
+ </tile>
+</tileset>
